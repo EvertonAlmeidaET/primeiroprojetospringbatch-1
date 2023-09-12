d22cd8a9bb9d9d1c857735c83cd164036d3b4a4a
Everton de Almeida <everton17@outlook.com>
2023-09-12 01:45:31 -0300

    first commit

---
+++ src/main/java/br/com/udemy/primeiroprojetospringbatchV1/Primeiroprojetospringbatch1Application.java
@@ -0,0 +1,13 @@
+package br.com.udemy.primeiroprojetospringbatchV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Primeiroprojetospringbatch1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Primeiroprojetospringbatch1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/udemy/primeiroprojetospringbatchV1/Primeiroprojetospringbatch1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy.primeiroprojetospringbatchV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Primeiroprojetospringbatch1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
